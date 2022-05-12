7d7d668bbd6e545c10a3948835390b3df5f64289
Matthieu Clay <matthieu.clay@outlook.fr>
2022-05-12 14:48:06 +0200

    :tada: initial commit

---
+++ src/main/java/com/matthieu/sii/coronavirustracker/CoronavirusTrackerApplication.java
@@ -0,0 +1,13 @@
+package com.matthieu.sii.coronavirustracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoronavirusTrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CoronavirusTrackerApplication.class, args);
+	}
+
+}
